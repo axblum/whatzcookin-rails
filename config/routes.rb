3d90067a822e8140a479d8c575adfc8953cfbf91
CJ & Nicole Yee <pair+cjoulain.themcny@devbootcamp.com>
2016-01-08 15:09:02 -0800

    Add in some google oauth - need to add keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   # resources 'users'
   devise_scope :user do
     get '/register' => 'devise/registrations#new'
