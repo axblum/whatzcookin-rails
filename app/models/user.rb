2a1eb27b818ba0dd15b6619f7486fa86b618ab48
CJ & Nicole Yee <pair+cjoulain.themcny@devbootcamp.com>
2016-01-08 14:49:54 -0800

    Begin google oauth process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  devise :omniauthable
   validates_presence_of :email, :encrypted_password
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
