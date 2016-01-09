d6e8a15bf7dbac5b0b2ef606ec9ac4251063f8c4
Nicole Yee <n.lindsey.yee@gmail.com>
2016-01-09 09:40:05 -0800

    Add comments to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 10.times do
-  User.create!(email: Faker::Internet.email,
-                  password: '12345678')
+  User.create!(name: Faker::Name.name,
+               email: Faker::Internet.email,
+               password: '12345678')
+end
+
+
+20.times do
+  Comment.create!(text: Faker::Lorem.paragraph,
+                  user_id: rand(1..10))
 end
