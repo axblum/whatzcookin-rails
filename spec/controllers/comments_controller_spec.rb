0ac1148907e3f10531cf95f82181e1b0a7510d02
CJ <email": null>
2016-01-11 11:21:41 -0800

    Create RSPEC features folder, install proper testing gems, and get started on testing for comment-user functionality.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe CommentsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(202)
     end
   end
 
   describe "GET #edit" do
     it "returns http success" do
       get :edit
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(202)
     end
   end
 
--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -3,12 +3,45 @@ require 'rails_helper'
 RSpec.feature "Comments", type: :feature do
   let(:user) { FactoryGirl.create :user}
   let!(:comment) { FactoryGirl.create :comment}
+
+ feature 'any visitor can view a recipe' do
+  scenario 'and a user does not have to log in' do
+    visit recipe_path
+    expect(page).to have_content(:welcome)
+  end
 end
 
 feature 'user is logged in' do
-  scenario 'can access the login page' do
+  scenario 'and can access the login page' do
     visit new_user_session_path
+    #fill_in 'Name', :with => user.name
     fill_in 'Email', :with => user.email
-    fill_in 'Password', :with => user.encrypted_password
+    fill_in 'Password', :with => user.password
+
+    click_button 'Log in'
+  end
+
+  scenario 'and can view a comment' do
+    visit recipe_path(recipe.id)
+    expect(page).to have_content(:comment)
+  end
+
+  scenario 'and can create a comment' do
+    visit new_recipe_comment_path
+    fill_in :with => :text
+    click_on "Save"
+      expect(page).to have_content(:comment)
+  end
+  # scenario 'can edit a comment' do
+  #   visit edit_recipe_comment do
+  #  click_on 'Save'
+
+    # scenario 'can delete a comment' do
+    # visit delete_recipe_comment do
+    # click_on 'Delete'
+
+    # scenario 'can comment on a comment' do
+    # path will depend on comment ID
+    # click_on 'Save'
   end
 end
