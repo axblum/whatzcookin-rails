701744c1dce7a5573a260ef9b296bc040bd76c7d
Daniel Woznicki <tokay89@sbcglobal.net>
2016-01-10 17:43:37 -0800

    Ajax edit buttons

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -6,33 +6,55 @@ function bindListeners() {
   $(".new-comment-form").on("submit", "#new_comment", function(e) {
     e.preventDefault();
     var data = $(this).serialize();
-    var action = $(this).attr("action")
-    createComment(data, action);
+    var url = $(this).attr("action")
+    createComment(data, url);
   });
-  $("li").on("click", ".edit-comment-form", function(e) {
-  debugger
+  $("li").on("click", ".edit-comment-button", function(e) {
     e.preventDefault();
-    var action = $(this).attr("action")
-    editComment(actions);
+    var url = $(this).attr("href");
+    $(this).toggle();
+    editComment(url);
   });
+  $("li").on("submit", ".edit_comment", function(e) {
+    e.preventDefault();
+    var data = $(this).serialize();
+    var url = $(this).attr("action");
+    updateComment(data, url);
+  })
 };
 
-function createComment(data, action) {
+function createComment(data, url) {
   $.ajax({
     method: 'POST',
-    url: action,
-    data: data,
+    url: url,
+    data: data
   })
-  .done(function(response){
+  .done(function(response) {
     $('.comments ul').append(response)
     $('.new_comment textarea').val("")
   });
 };
 
-function editComment(action) {
+function editComment(url) {
   $.ajax({
     method: 'GET',
-    url: action + '/edit'
+    url: url
   })
+  .done(function(response) {
+    var id = this.url.split("/")[4]
+    $("li#" + id).append(response);
+  });
 };
 
+function updateComment(data, url) {
+  $.ajax({
+    method: 'PATCH',
+    url: url,
+    data: data
+  })
+  .done(function(response) {
+    $("li#" + response.id).children().first().text(response.text);
+    $("li#" + response.id + " form").hide();
+    $("li#" + response.id + " .edit-comment-button").toggle();
+  });
+};
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 
   def edit
+    @comment = Comment.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
     if request.xhr?
       render partial: "/comments/form"
     else
@@ -9,17 +11,23 @@ class CommentsController < ApplicationController
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment.update_attributes(comment_params)
+    if @comment.save
+      if request.xhr?
+        render json: @comment
+      end
+    end
   end
 
   def create
-    p "-" * 50
-    p comment_params
-    @recipe = Recipe.find_or_create_by(api_id: params[:recipe_id])
     @comment = Comment.new(comment_params)
+    @recipe = Recipe.find(params[:recipe_id])
     @comment.update_attributes(user_id: current_user.id, recipe_id: params[:recipe_id])
     if @comment.save
       if request.xhr?
-        render partial: "/comments/comment", locals: {comment: @comment, recipe: @recipe}
+        render partial: "/comments/comment"
       end
     end
   end
